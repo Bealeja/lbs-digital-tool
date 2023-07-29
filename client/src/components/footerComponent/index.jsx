8ba82b64bfc9861a6099b8a2144cdf62919aafa1
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-07-29 21:43:54 +1000

    Updated Home Page

---
+++ client/src/components/footerComponent/index.jsx
@@ -0,0 +1,31 @@
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="flex-center">
+        <h2>Contact Us</h2>
+        <h3>hello@littlebigsteps.org.au</h3>
+        <div>
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faFacebook} class="anchor-icon" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} class="anchor-icon" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faTwitter} class="anchor-icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/tableComponent/index.jsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+const Table = () => {
+  return (
+    <div class="chart">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  );
+};
+
+export default Table;
