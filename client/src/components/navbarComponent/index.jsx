16d1b44cee0dc687820834c1abf2c16a7f42637b
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-08-06 12:33:55 +1000

    Issues updated for main home page

--- client/src/components/navbarComponent/index.jsx
+++ client/src/components/navbarComponent/index.jsx
@@ -1,21 +1,51 @@
 import { Link } from "react-router-dom";
-import "../../App.scss";
+import "./index.scss";
 
 const Navbar = () => {
   return (
-    <nav>
+    <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/statistics">About</Link>
-        </li>
-        <li>
-          <Link to="/donations">Donate</Link>
+          <a href="">
+            <span>
+              <svg
+                xmlns="http:www.w3.org/2000/svg"
+                width={192}
+                height={192}
+                fill="currentColor"
+                viewBox="0 0 256 256"
+              >
+                <rect width={256} height={256} fill="none" />
+                <circle
+                  cx={128}
+                  cy={128}
+                  r={96}
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={16}
+                />
+              </svg>
+            </span>
+          </a>
         </li>
       </ul>
-    </nav>
+    </div>
+
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/statistics">About</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/donations">Donate</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 };
 
