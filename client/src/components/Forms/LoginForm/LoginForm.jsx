0cc6850e9993cb9e59899210e6bf6c504f2f00e0
Bealja <117786111+Bealeja@users.noreply.github.com>
2023-09-16 17:38:05 +1000

    login page refactored

--- client/src/components/Forms/LoginForm/LoginForm.jsx
+++ client/src/components/Forms/LoginForm/LoginForm.jsx
@@ -3,6 +3,20 @@ import * as yup from "yup";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 //Create register and login schemas for yup validation
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -102,84 +116,102 @@ const Form = () => {
     if (isLogin) await login(values, onSubmitProps);
     if (isRegister) await register(values, onSubmitProps);
   };
-
+  const defaultTheme = createTheme();
   return (
-    <>
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
-        validationSchema={isLogin ? loginSchema : registerSchema}
-      >
-        {/* handleBlur: handles what has been touched
-            handleChange: updates form values based on what was changed 
-            handleSubmit: Triggers whatever is in the onSubmit when type=submit elements are triggered,
-            resetForm: Resets the form
-            */}
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-          resetForm,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            {/* IF State:pageType === "register" -> display register details ELSE display login details */}
-            <div class="signup">
-              <label for="chk" aria-hidden="true">
-                {isLogin ? "LOGIN" : "SIGN UP"}
-              </label>
-              {isRegister && (
-                <>
-                  <input
-                    placeholder="First Name"
-                    label="First Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.firstName}
-                    name="firstName"
-                  />
-                  <input
-                    placeholder="Last Name"
-                    label="Last Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.lastName}
-                    name="lastName"
-                  />
-                </>
-              )}
-
-              <input
-                placeholder="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-              />
-              <input
-                placeholder="Password"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                name="password"
-              />
-              <button type="submit">{isLogin ? "LOGIN" : "REGISTER"}</button>
-              <h6
-                onClick={() => {
-                  setPageType(isLogin ? "register" : "login");
-                }}
-              >
-                {isLogin
-                  ? "Don't have an account? Sign Up here."
-                  : "Already have an account? Login here."}
-              </h6>
-            </div>
-          </form>
-        )}
-      </Formik>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleFormSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- client/src/scenes/Public/loginPage/loginPage.jsx
+++ client/src/scenes/Public/loginPage/loginPage.jsx
@@ -5,17 +5,7 @@ import LoginForm from "../../../components/Forms/LoginForm/LoginForm";
 const LoginPage = () => {
   return (
     <>
-      <Navbar />
-      <div class="grid-container">
-        <div class="grid-row-str-1">
-          <div class="flex-row center-items">
-            <div class="main">
-              <input type="checkbox" id="chk" aria-hidden="true" />
-              <LoginForm />
-            </div>
-          </div>
-        </div>
-      </div>
+      <LoginForm />
     </>
   );
 };
