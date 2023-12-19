8bc0fff4d4b6bff052e3eee549984f488e3c9f47
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-12-19 19:12:39 +1100

    members and jobs UI fixed

--- client/src/scenes/Public/activeFundraiserPage/activeFundraiserPage.jsx
+++ client/src/scenes/Public/activeFundraiserPage/activeFundraiserPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 
 const ActiveFundraiserPage = () => {
@@ -100,19 +100,17 @@ const ActiveFundraiserPage = () => {
           </Box>
         </Grid>
 
-        <Grid
-          item
-          sx={{ width: "30%", backgroundColor: "blue", height: "fit-content" }}
-        >
+        <Grid item sx={{ width: "30%", height: "88vh" }}>
           <Box
             sx={{
               backgroundColor: "white",
               boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
-              height: "100%",
+              height: "50%",
               borderRadius: "8px",
               padding: "10px",
               display: "flex",
-              flexDirection: "row",
+              flexDirection: "column",
+              marginBottom: "10px",
             }}
           >
             <Typography
@@ -127,12 +125,59 @@ const ActiveFundraiserPage = () => {
             >
               Members
             </Typography>
+            <Box
+              sx={{
+                height: "fit-content",
+                width: "100%",
+                marginTop: "10px",
+              }}
+            >
+              {members?.map((member, i) => (
+                <Box
+                  sx={{
+                    backgroundColor: "white",
+                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
+                    height: "fit-content",
+                    borderRadius: "8px",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      sx={{
+                        color: "#b2b2b2",
+                        fontSize: "14pt",
+                        fontFamily: "sans-serif",
+                        fontWeight: "bold",
+                        marginRight: "10px",
+                        textShadow: "0.5px 0.5px 0px rgba(0, 0, 0, 0.5)",
+                      }}
+                    >
+                      {member}
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Avatar
+                      src={`http://localhost:3001/assets/eventcoverphotos/${photo}`}
+                      alt="Profile Photo"
+                      variant="rounded"
+                      sx={{ height: "50px", width: "50px" }}
+                    />
+                  </Box>
+                </Box>
+              ))}
+            </Box>
           </Box>
+
+          {/* Jobs */}
           <Box
             sx={{
               backgroundColor: "white",
               boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
-              height: "fit-content",
+              height: "50%",
               borderRadius: "8px",
               padding: "10px",
               display: "flex",
@@ -153,11 +198,12 @@ const ActiveFundraiserPage = () => {
             </Typography>
             <Box
               sx={{
-                height: "200px",
+                height: "fit-content",
                 width: "100%",
+                marginTop: "10px",
               }}
             >
-              {jobs?.map((job, i) => (
+              {Object.keys(jobs)?.map((job, i) => (
                 <Box
                   sx={{
                     backgroundColor: "white",
@@ -166,20 +212,48 @@ const ActiveFundraiserPage = () => {
                     borderRadius: "8px",
                     padding: "10px",
                     marginBottom: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
                   }}
                 >
-                  <Typography
-                    sx={{
-                      color: "#b2b2b2",
-                      fontSize: "10pt",
-                      fontFamily: "sans-serif",
-                      fontWeight: "bold",
-                      marginRight: "10px",
-                      textShadow: "0.5px 0.5px 0px rgba(0, 0, 0, 0.5)",
-                    }}
-                  >
-                    {job}
-                  </Typography>
+                  <Box>
+                    <Typography
+                      sx={{
+                        color: "#b2b2b2",
+                        fontSize: "14pt",
+                        fontFamily: "sans-serif",
+                        fontWeight: "bold",
+                        marginRight: "10px",
+                        textShadow: "0.5px 0.5px 0px rgba(0, 0, 0, 0.5)",
+                      }}
+                    >
+                      {job}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        color: "#9eb9d1",
+                        fontSize: "10pt",
+                        fontFamily: "sans-serif",
+                        fontWeight: "bold",
+                        marginRight: "10px",
+                        textShadow: "0.5px 0.5px 0px rgba(0, 0, 0, 0.5)",
+                      }}
+                    >
+                      Assigned To: {jobs[job] ? jobs[job] : "N/A"}
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Avatar
+                      src={
+                        jobs[job]
+                          ? `http://localhost:3001/assets/eventcoverphotos/${photo}`
+                          : ""
+                      }
+                      alt="Profile Photo"
+                      variant="rounded"
+                      sx={{ height: "50px", width: "50px" }}
+                    />
+                  </Box>
                 </Box>
               ))}
             </Box>
