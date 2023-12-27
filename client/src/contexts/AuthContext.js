8384a2d7ffd5d270bb070a9c6acb4f0b3229910b
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-12-27 20:33:07 +1100

    error with firebase projectId

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { auth } from "../firebase";
+import { auth } from "../utility/firebase.js";
 
 const AuthContext = React.createContext();
 
